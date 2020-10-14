21b130ece1a2036520c0287e2ff6904a94ba2f3f
aakarsh30 <aakarshsaxenasup@gmail.com>
2020-10-15 03:00:10 +0530

    hostfinal2

--- build/precache-manifest.a67d53dbbca271872dafc3d3613c203f.js
+++ build/precache-manifest.a67d53dbbca271872dafc3d3613c203f.js
@@ -1,27 +1,39 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d1f208209c703406b9d39d42bcee73f",
+    "revision": "276276c439091e03a1d4b6466bb01d01",
     "url": "/auth/index.html"
   },
   {
-    "revision": "26720afdf51c7f3319d5",
+    "revision": "6b3cdba1c58109bb30df",
     "url": "/auth/static/css/main.aad090c1.chunk.css"
   },
   {
-    "revision": "6e61f9d177cd860a6dc7",
-    "url": "/auth/static/js/2.39ea773f.chunk.js"
+    "revision": "5853549b9080da309a83",
+    "url": "/auth/static/js/2.7f97efb1.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/auth/static/js/2.39ea773f.chunk.js.LICENSE.txt"
+    "url": "/auth/static/js/2.7f97efb1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "26720afdf51c7f3319d5",
-    "url": "/auth/static/js/main.768d5bf5.chunk.js"
+    "revision": "6b3cdba1c58109bb30df",
+    "url": "/auth/static/js/main.23b3e32a.chunk.js"
   },
   {
     "revision": "abb45201b89394e19588",
     "url": "/auth/static/js/runtime-main.9d73ae04.js"
+  },
+  {
+    "revision": "30ccc0698d42761f11da05832fcd347f",
+    "url": "/auth/static/media/1.30ccc069.jpg"
+  },
+  {
+    "revision": "693d30fae50972be1cddafdc800de9de",
+    "url": "/auth/static/media/2.693d30fa.jpg"
+  },
+  {
+    "revision": "d0b77ddd061f860f05b5b107a65c1498",
+    "url": "/auth/static/media/3.d0b77ddd.jpg"
   },
   {
     "revision": "d808776978a0052988b117a7275eabd3",
@@ -30,6 +42,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "33fabf03da1085eb682819527dfee6cf",
     "url": "/auth/static/media/comp.33fabf03.png"
+  },
+  {
+    "revision": "da7951e7ea8cadee29e7686011aef9ba",
+    "url": "/auth/static/media/i.da7951e7.png"
+  },
+  {
+    "revision": "1cc940221a24534075718dae3de2ddf5",
+    "url": "/auth/static/media/ii.1cc94022.png"
   },
   {
     "revision": "187b4de4402e164cb3c0db59080c4940",
@@ -46,5 +66,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "292cfd1a1f6c50f582123e11d6b0b970",
     "url": "/auth/static/media/s4.292cfd1a.png"
+  },
+  {
+    "revision": "50ceecc39183f18ed6c0313e09de1e9c",
+    "url": "/auth/static/media/vii.50ceecc3.png"
   }
 ]);
